b7ed529e53c3c641fecde2c28721e876fb278dd5
NicolaeArhire <nicolae.arhire10@gmail.com>
2023-06-22 02:52:16 +0300

    Log Out User Due To Inactivity

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [displayCartProducts, setDisplayCartProducts] = useState(0);
   const [cartCoordinates, setCartCoordinates] = useState({ x: 0, y: 0 });
+  const [periodOfInactivity, setPeriodOfInactivity] = useState(7 * 24 * 60 * 60 * 1000); // 7 days in miliseconds
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -26,6 +27,38 @@ function App() {
 
     return () => unsubscribe();
   }, []); // display cart products based on logged in status
+
+  const currentUser = auth.currentUser;
+
+  useEffect(() => {
+    const userLogOut = () => {
+      auth
+        .signOut()
+        .then(() => {
+          console.log("User logged out due to inactivity.");
+        })
+        .catch((error) => {
+          console.error("Error logging out user:", error);
+        });
+    };
+
+    if (currentUser) {
+      const resetPeriodOfInactivity = () => {
+        setPeriodOfInactivity(7 * 24 * 60 * 60 * 1000);
+      };
+
+      // call the function when "periodOfInactivity" is 0, which after every click/ mousemove is turned back to 7 days
+      setTimeout(userLogOut, periodOfInactivity);
+
+      document.addEventListener("mousemove", resetPeriodOfInactivity);
+      document.addEventListener("click", resetPeriodOfInactivity);
+    }
+
+    return () => {
+      document.removeEventListener("mousemove", setPeriodOfInactivity(7 * 24 * 60 * 60 * 1000));
+      document.removeEventListener("click", setPeriodOfInactivity(7 * 24 * 60 * 60 * 1000));
+    };
+  }, [currentUser]);
 
   return (
     <div className="app">
